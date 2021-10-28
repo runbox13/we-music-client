27d4f24e9f8e6960d5677d92a5cecc8879412e8b
simon <39092572+runbox13@users.noreply.github.com>
2021-10-29 00:31:01 +1100

    Update chatroom.js

--- src/page/chatroom.js
+++ src/page/chatroom.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 import '../css/chatroom.css';
 
 
-const socket = new W3CWebSocket('ws://127.0.0.1:8080');
+const socket = new W3CWebSocket('ws://websocket420.herokuapp.com:8080');
 
 function SidebarPlaylist(props) {
     var tracks = [];
